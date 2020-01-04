03254c00c406381e2bcbc6be244f3bcbbcb5cb8c
defMyself <anxinyucomputer@163.com>
2020-01-04 17:22:44 +0800

    concatnating String function

---
+++ 1-17ConcatenatingString.cpp
@@ -0,0 +1,7 @@
+int main()
+{
+    const string s = "Pease-Porridge ";
+    string nurseryRhyme = s + "hot, " + s + "cold,\n";
+    nurseryRhyme += s + "in the pot, nine days old.";
+    cout << nurseryRhyme;
+}
